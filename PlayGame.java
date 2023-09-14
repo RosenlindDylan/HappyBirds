18f500f514aae73144c21255e1bf3cd4a88eb7eb
RosenlindDylan <35314066+RosenlindDylan@users.noreply.github.com>
2023-09-14 15:30:56 -0700

    fire / path methods

--- PlayGame.java
+++ PlayGame.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 
 public class PlayGame {
 
-    private double[][] environment;
-    private Bird bird;
+    public static boolean[][] environment;
+    private static Bird bird;
     Scanner input = new Scanner(System.in);
     // later constants for env?
 
     // Constructor
     public PlayGame() {
-        environment = new double[100][100]; 
+        environment = new boolean[100][100]; 
         bird = new Bird();
-
     }
 
     // aim angle in degrees, convert to slope
-    // param:
+    // params:
     //      aimAngle 0 <= theta <= 90
+    //      0 < power < something else i guess
     public void fire(int aimAngle, int power) {
         // param check
         while (aimAngle > 90 || aimAngle < 0) {
@@ -28,33 +28,50 @@ public class PlayGame {
        }
        
        // while something, update tick time and call path
+
+       // convert degrees -> radians
+       double angle = Math.toRadians(aimAngle);
        
         // componentize power vector
-        double v_0x = power * Math.sin(aimAngle);
-        double v_0y = power * Math.cos(aimAngle);
-       
-        path(aimAngle, power, v_0x, v_0y);
+        double v_0x = power * Math.cos(angle);
+        double v_0y = power * Math.sin(angle);
+
+        System.out.println("x initial is: " + v_0x);
+        System.out.println("y initial is " + v_0y);
+
+        int time = 1;
+
+        while (path(power, v_0x, v_0y, time)) {
+            time++;
+        }
 
         System.out.println("The bird hit " + bird.getCords() + "!" );
     }
 
-    private static void path(int aimAngle, int power, double v_0x, double v_0y) {
-        
-        // placeholder time variable
-        int time = 1;
+    private static boolean path(int power, double v_0x, double v_0y, int time) {
+
+        double tick = time / 100.0;
 
         // use kinematic equ to update x and y
-        double newX = v_0x * time;
-        double newY = v_0y * time + (1.0 / 2) * -9.81 * Math.pow(time,2);
+        double newX = v_0x * tick;
+        double newY = v_0y * tick + (1.0 / 2) * -9.81 * Math.pow(tick,2);
+        
+        // update the bird cords
+        bird.updateCords(newX, newY);
 
+        // pixel cordinates
+        int x = (int)(Math.round(newX));
+        int y = (int)(Math.round(newY));
 
+        if (x >= 100 || y >= 100 || x < 0 || y < 0 || environment[x][y]) {
+            System.out.println("the bird hit something!");
+            return false;
+        }
 
+        System.out.println(newX + " " + newY);
+        return true;
     
     }
-
-
-
-
 
     public void getCords() {
         bird.getCords();
@@ -63,8 +80,9 @@ public class PlayGame {
 
     public static void main(String[] args) {
         PlayGame game = new PlayGame();
-        game.fire(100, 100);
+        game.fire(30, 20);
     }
+
 }
 
 
