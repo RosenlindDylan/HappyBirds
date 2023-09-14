666426a74e1f8d5069ff9b56386945479ec0aafa
RosenlindDylan <35314066+RosenlindDylan@users.noreply.github.com>
2023-09-14 14:12:50 -0700

    update path method

--- PlayGame.java
+++ PlayGame.java
@@ -17,32 +17,40 @@ public class PlayGame {
     // aim angle in degrees, convert to slope
     // param:
     //      aimAngle 0 <= theta <= 90
-    public void fire(int aimAngle) {
+    public void fire(int aimAngle, int power) {
         // param check
         while (aimAngle > 90 || aimAngle < 0) {
             System.out.println("Aim angle must be between 0 and 90 degrees! Please enter a new angle:");
             // parseint might b a dumb way to cast it to an integer
             // prolly catch a non integer exception up here?
-            // int angle = Integer.parseInt(input.nextLine());
-            
-        }
-
-        // convert to first quadrant slope
-        double slope = aimAngle / 90.0;
-
-        if (aimAngle < 45) {
-            // hits side first so x = 100 and calculate y
-            double y = Math.round(slope * 100);
-            bird.updateCords(100, y);
-        } else {
-            // hits top first
-            double x = Math.round(slope * 100);
-            bird.updateCords(x, 100);
-        }        
+            aimAngle = Integer.parseInt(input.nextLine());
+            // regex check instead for AN
+       }
+       
+       // while something, update tick time and call path
+       
+        // componentize power vector
+        double v_0x = power * Math.sin(aimAngle);
+        double v_0y = power * Math.cos(aimAngle);
+       
+        path(aimAngle, power, v_0x, v_0y);
 
         System.out.println("The bird hit " + bird.getCords() + "!" );
     }
 
+    private static void path(int aimAngle, int power, double v_0x, double v_0y) {
+        
+        // placeholder time variable
+        int time = 1;
+
+        // use kinematic equ to update x and y
+        double newX = v_0x * time;
+        double newY = v_0y * time + (1.0 / 2) * -9.81 * Math.pow(time,2);
+
+
+
+    
+    }
 
 
 
@@ -55,7 +63,7 @@ public class PlayGame {
 
     public static void main(String[] args) {
         PlayGame game = new PlayGame();
-        game.fire(100);
+        game.fire(100, 100);
     }
 }
 
